93f6acdcb4671a41253847dde4405b7ad814b404
Gustavo K. <gustavoklc1510@gmail.com>
2025-04-15 19:06:33 -0300

    Comentei Atividade3.java

--- atividade3_logprog/src/atividade3_logprog/Atividade3.java
+++ atividade3_logprog/src/atividade3_logprog/Atividade3.java
@@ -6,13 +6,13 @@ public class Atividade3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		// Pede dois números ao usuário
 		System.out.println("Digite dois números inteiros: ");
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
-		
+		// faz a subtração
 		int sub = n1-n2;
-		
+		// Mostra os resultados
 		if (sub<0)
 		{
 			int sub2 = sub*-1;
