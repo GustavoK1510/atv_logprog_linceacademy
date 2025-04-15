35d348a48024f8b25b1798b9e9f4b721faa9217b
Gustavo K. <gustavoklc1510@gmail.com>
2025-04-15 19:02:38 -0300

    Comentei Atividade1.java

--- atividade1_logprog/src/atividade1_logprog/Atividade1.java
+++ atividade1_logprog/src/atividade1_logprog/Atividade1.java
@@ -6,16 +6,16 @@ public class Atividade1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		//Pede dois números ao usuário
 		System.out.println("Digite dois números decimais: ");
 		float n1 = sc.nextFloat();
 		float n2 = sc.nextFloat();
-		
+		// se n1 for maior que n2, faz a divisão entre eles
 		if (n1>n2) 
 		{
 			System.out.println(n1/n2);			
 		}
-		else 
+		else // e aqui é o outro caso
 		{
 			System.out.println(n2/n1);
 		}
